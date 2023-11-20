d3e6c4138ab219bb827219f0ff704be827a22d48
Will <wimveto@protonmail.com>
2023-11-20 06:45:55 -0300

    initial commit

--- config.c
+++ config.c
@@ -4,16 +4,19 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-mail",    600,  1 },
-    {"sb-music",   0,    2 },
-    {"sb-disk",    1800, 3 },
-    {"sb-memory",  10,   4 },
-    {"sb-loadavg", 5,    5 },
-    {"sb-mic",     0,    6 },
-    {"sb-record",  0,    7 },
-    {"sb-volume",  0,    8 },
+    {"cat /tmp/recordingicon 2>/dev/null",	0,	1},
+    {"sb-music",   18000,    2 },
+    /*{"sb-disk",    1800, 3 },*/
+    /*{"sb-memory",  10,   4 },*/
+    /*{"sb-news", 0,    5 },*/
+    /*{"sb-forecast",     18000,    6 },*/
+    /*{"sb-nettraf",  1,    7 },*/
+    {"sb-volume",  18000,    8 },
     {"sb-battery", 5,    9 },
-    {"sb-date",    1,    10},
+    {"sb-clock",    1,    10},
+    {"sb-internet",    1,    11},
+    /*{"sb-help-icon",    0,    12},*/
 };
 
 const unsigned short blockCount = LEN(blocks);
+
